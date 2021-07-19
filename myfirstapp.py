621b16552292cd07fbc1fa0e975fa6da3d46aea7
ShankerRamasamy <87532052+ShankerRamasamy@users.noreply.github.com>
2021-07-19 21:07:05 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -12,15 +12,6 @@ st.write(pd.DataFrame({
     'second column': [10, 20, 30, 40]
 }))
 
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
 
 df
 
